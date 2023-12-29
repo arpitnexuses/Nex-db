3a3284f3ab1c28d7ec039c723edc3cf2f6b27d3f
arpitnexuses <107392818+arpitnexuses@users.noreply.github.com>
2023-12-29 16:11:36 +0530

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -11,26 +11,118 @@ st.set_page_config(layout='wide', initial_sidebar_state='collapsed')
 
 # Connect to MongoDB using environment variable
 mongodb_uri = os.getenv("MONGODB_URI")
+client = pymongo.MongoClient(mongodb_uri,tlsCAFile=certifi.where())
+db = client["mydatabase"]
+users_col = db["users"]
 
-try:
-    client = pymongo.MongoClient(mongodb_uri, tlsCAFile=certifi.where())
-    db = client["mydatabase"]
-    users_col = db["users"]
-except pymongo.errors.ConnectionFailure as e:
-    st.error(f"Error connecting to MongoDB: {e}")
-    st.stop()  # Stop execution to avoid further issues
 
 # Initialize the 'authenticated' attribute
 if "authenticated" not in st.session_state:
-    st.session_state['authenticated'] = False
+  st.session_state['authenticated'] = False
 
-# Load external CSS
 with open('styles.css') as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+  st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+# Connect to MongoDB
+
+
+# with open('styles.css') as f:
+#     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+CSS = """
+h1 {
+    color: #FFFFFF;
+    font-size: 48px;
+    font-weight: bold;
+    text-align: center;
+    margin-top: 100px;
+}
+"""
+
+
+# Create login page
+def login():
+  st.write("<h1>Login</h1>", unsafe_allow_html=True)
+  username = st.text_input("Username")
+  password = st.text_input("Password", type="password")
+  if st.button("Login"):
+    user = users_col.find_one({"username": username, "password": password})
+    if user:
+      st.session_state.authenticated = True
+      st.experimental_rerun()  # Refresh the app to redirect to dashboard
+    else:
+      st.error("Incorrect username or password")
+
+
+css = """
+<style>
+    .login-container {
+        padding: 20px;
+        border: 1px solid #ccc;
+        border-radius: 10px;
+        background-color: #f7f7f7;
+        max-width: 400px;
+        margin: auto;
+        margin-top: 40px;
+    }
+</style>
+"""
+st.markdown(css, unsafe_allow_html=True)
+
+
+# Create register page
+def register():
+  if st.session_state.is_admin:
+    if st.session_state.show_register_form:
+      st.write("Click the button below to register a new user.")
+      if st.button("Register a new user"):
+        st.session_state.show_register_form = True
+    else:
+      st.write("Please fill out the form to register a new user:")
+      username = st.text_input("Username")
+      password = st.text_input("Password", type="password")
+      email = st.text_input("Email")
+      if st.button("Register"):
+        user = {"username": username, "password": password, "email": email}
+        users_col.insert_one(user)
+        st.session_state.show_register_form = False
+        st.success("User registered successfully!")
+  else:
+    st.write("You are not authorized to register a new user.")
+    st.experimental_rerun()  # Refresh the app to redirect to dashboard
+
+
+# Create dashboard page
+def dashboard():
+  if st.session_state.authenticated:
+    with st.spinner("Loading..."):
+      #
+      st.title("Welcome")
+  else:
+    st.write("Please login to access the dashboard.")
+    login()
+
+
+def sidebar():
+  # Add your sidebar elements here
+  pass
+
+
+is_admin = False  # Set to True if user is an admin
+
+if 'authenticated' in st.session_state and st.session_state['authenticated']:
+
+  if is_admin:
+    if st.button("Register"):
+      register()
+  dashboard()
+else:
+  login()
 
 # CSS styling
 st.markdown("""
-<style>
+    <style>
+    
+
     .login-container {
         background-color: #45b6fe;
         border-radius: 8px;
@@ -50,59 +142,7 @@ st.markdown("""
     .stButton button:hover {
         background-color: #1f8bf8;
     }
-</style>
-""", unsafe_allow_html=True)
-
-# Create login page
-def login():
-    st.title("Login")
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
-    if st.button("Login"):
-        try:
-            user = users_col.find_one({"username": username, "password": password})
-            if user:
-                st.session_state.authenticated = True
-                st.experimental_rerun()  # Refresh the app to redirect to dashboard
-            else:
-                st.error("Incorrect username or password")
-        except pymongo.errors.PyMongoError as e:
-            st.error(f"Error querying MongoDB: {e}")
-
-# Create register page
-def register():
-    if st.session_state.is_admin:
-        st.write("Click the button below to register a new user.")
-        if st.button("Register a new user"):
-            st.session_state.show_register_form = True
-    else:
-        st.write("Please fill out the form to register a new user:")
-        username = st.text_input("Username")
-        password = st.text_input("Password", type="password")
-        email = st.text_input("Email")
-        if st.button("Register"):
-            try:
-                user = {"username": username, "password": password, "email": email}
-                users_col.insert_one(user)
-                st.session_state.show_register_form = False
-                st.success("User registered successfully!")
-            except pymongo.errors.PyMongoError as e:
-                st.error(f"Error inserting into MongoDB: {e}")
-
-# Create dashboard page
-def dashboard():
-    if st.session_state.authenticated:
-        st.title("Welcome")
-    else:
-        st.write("Please login to access the dashboard.")
-        login()
-
-is_admin = False  # Set to True if the user is an admin
-
-if 'authenticated' in st.session_state and st.session_state['authenticated']:
-    if is_admin:
-        if st.button("Register"):
-            register()
-    dashboard()
-else:
-    login()
+    
+    </style>
+    """,
+            unsafe_allow_html=True)
