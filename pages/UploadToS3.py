c613f556f736fd1f5c37f8a5b5b36e8839378b16
arpitnexuses <107392818+arpitnexuses@users.noreply.github.com>
2024-01-01 19:14:47 +0530

    Update UploadToS3.py

--- pages/UploadToS3.py
+++ pages/UploadToS3.py
@@ -7,15 +7,13 @@ from dotenv import load_dotenv
 import os
 
 st.set_page_config(layout='wide', initial_sidebar_state='expanded')
-with open('style.css') as f:
-  st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+with open('file/style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 if "authenticated" not in st.session_state:
-  st.session_state.authenticated = False
+    st.session_state.authenticated = False
+
 # Connect to MongoDB
-
-
-# Load environment variables from .env file
 load_dotenv()
 
 # MongoDB connection
@@ -37,64 +35,63 @@ s3 = boto3.client('s3',
                   region_name=aws_region)
 
 def get_object_url(file_name, bucket_name):
-  try:
-    url = f"https://{bucket_name}.s3.{AWS_REGION}.amazonaws.com/{file_name}"
-    return url
-  except NoCredentialsError:
-    return 'AWS credentials not available.'
-
+    try:
+        url = f"https://{bucket_name}.s3.{aws_region}.amazonaws.com/{file_name}"
+        return url
+    except NoCredentialsError:
+        return 'AWS credentials not available.'
 
 def main():
-  if st.session_state.authenticated:
-    st.title("Upload and Get S3 Object URL")
+    if st.session_state.authenticated:
+        st.title("Upload and Get S3 Object URL")
 
-    selected_bucket = st.selectbox("Select a S3 bucket:", S3_BUCKET_NAMES)
+        selected_bucket = st.selectbox("Select a S3 bucket:", s3_bucket_names)
 
-    uploaded_file = st.file_uploader(
-      "Choose a file...",
-      type=["jpg", "jpeg", "png", "gif", "mp4", "avi", "mkv", "pdf"])
+        uploaded_file = st.file_uploader(
+            "Choose a file...",
+            type=["jpg", "jpeg", "png", "gif", "mp4", "avi", "mkv", "pdf"])
 
-    if uploaded_file is not None:
-      file_name = st.text_input(
-        "Enter a file name (including extension):",
-        value=uploaded_file.name if uploaded_file else '')
+        if uploaded_file is not None:
+            file_name = st.text_input(
+                "Enter a file name (including extension):",
+                value=uploaded_file.name if uploaded_file else '')
 
-      if uploaded_file.type.startswith('image'):
-        # Display image
-        st.image(uploaded_file,
-                 caption="Uploaded Image/Video/PDF.",
-                 use_column_width=True)
-      elif uploaded_file.type == 'application/pdf':
-        # Display a PDF link
-        st.markdown(
-          f"Uploaded PDF: [{file_name}]({get_object_url(file_name, selected_bucket)})"
-        )
-      else:
-        st.warning("Unsupported file type. Please upload an image or a PDF.")
+            if uploaded_file.type.startswith('image'):
+                # Display image
+                st.image(uploaded_file,
+                         caption="Uploaded Image/Video/PDF.",
+                         use_column_width=True)
+            elif uploaded_file.type == 'application/pdf':
+                # Display a PDF link
+                st.markdown(
+                    f"Uploaded PDF: [{file_name}]({get_object_url(file_name, selected_bucket)})"
+                )
+            else:
+                st.warning("Unsupported file type. Please upload an image or a PDF.")
 
-      if st.button("Get S3 Object URL"):
-        if not file_name:
-          st.warning("Please enter a file name.")
-        else:
-          # Upload the file to the selected S3 bucket with appropriate Content-Type
-          try:
-            content_type, _ = mimetypes.guess_type(file_name)
-            s3.upload_fileobj(uploaded_file,
-                              selected_bucket,
-                              file_name,
-                              ExtraArgs={'ContentType': content_type})
-            st.success("File uploaded to S3 successfully.")
-          except Exception as e:
-            st.error(f"Error uploading to S3: {str(e)}")
+            if st.button("Get S3 Object URL"):
+                if not file_name:
+                    st.warning("Please enter a file name.")
+                else:
+                    # Upload the file to the selected S3 bucket with appropriate Content-Type
+                    try:
+                        content_type, _ = mimetypes.guess_type(file_name)
+                        s3.upload_fileobj(uploaded_file,
+                                          selected_bucket,
+                                          file_name,
+                                          ExtraArgs={'ContentType': content_type})
+                        st.success("File uploaded to S3 successfully.")
+                    except Exception as e:
+                        st.error(f"Error uploading to S3: {str(e)}")
 
-          # Get the S3 object URL
-          object_url = get_object_url(file_name, selected_bucket)
-          st.write("S3 Object URL:")
-          st.write(object_url)
+                    # Get the S3 object URL
+                    object_url = get_object_url(file_name, selected_bucket)
+                    st.write("S3 Object URL:")
+                    st.write(object_url)
 
-  else:
-    st.error("Please login to access the dashboard.")
+    else:
+        st.error("Please login to access the dashboard.")
 
 
 if __name__ == "__main__":
-  main()
+    main()
