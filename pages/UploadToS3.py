335c1e932d8cbf29920c96561bf3d4868e63977e
arpitnexuses <107392818+arpitnexuses@users.noreply.github.com>
2024-07-10 18:19:38 +0530

    Update UploadToS3.py

--- pages/UploadToS3.py
+++ pages/UploadToS3.py
@@ -82,6 +82,7 @@ def main():
                     st.write("S3 Object URL:")
                     st.write(object_url)
                     st.snow()
+                    st.toast('Hooray!', icon='🎉')
 
 
     else:
